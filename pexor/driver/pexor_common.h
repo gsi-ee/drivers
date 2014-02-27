17a1d7c0fa7dc799a081ebe0e91cbc992fbac611
adamczew <adamczew@bcbf6573-9a26-0410-9ebc-ce4ab7aade96>
2014-02-27 09:08:34 +0000

    pexor driver: enable direct dma mode by default after it has proved to work
    git-svn-id: https://subversion.gsi.de/dabc/drivers@2036 bcbf6573-9a26-0410-9ebc-ce4ab7aade96

--- pexor/driver/pexor_common.h
+++ pexor/driver/pexor_common.h
@@ -56,11 +56,11 @@
 #define BOARDTYPE_KINPEX 2
 
 
-#define PEXOR_DEBUGPRINT 1
+//#define PEXOR_DEBUGPRINT 1
 
 // this will enable mode where pexor on board memory is not used as buffer
 // instead, data from sfp token request will be directly streamed to host dma buffer.
-//#define PEXOR_DIRECT_DMA 1
+#define PEXOR_DIRECT_DMA 1
 
 
 /* this define will switch at compiletime between trbnet and gosip protocols*/
