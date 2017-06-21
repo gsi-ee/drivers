bae4db976db39f1cd172fb76273c85b58dbc6f66
adamczew <adamczew@bcbf6573-9a26-0410-9ebc-ce4ab7aade96>
2017-06-21 12:38:37 +0000

    mbspex gui: improved makefile structure
    git-svn-id: https://subversion.gsi.de/dabc/drivers@3697 bcbf6573-9a26-0410-9ebc-ce4ab7aade96

---
+++ mbspex/gui/qt/gui.mk
@@ -0,0 +1,12 @@
+QMAKE = qmake-qt4
+
+INC = $(MBSROOT)/inc
+
+PEX_BASE=/mbs/driv/mbspex_$(GSI_OS_VERSION)_DEB
+PEX_BINPATH=$(PEX_BASE)/bin/
+PEX_LIBPATH=$(PEX_BASE)/lib/
+PEX_INCPATH=$(PEX_BASE)/include
+PEX_INC = -I$(PEX_INCPATH)
+PEX_LIBA = $(PEX_LIBPATH)/libmbspex.a
+
+CFLAGS  = $(POSIX) -DLinux -Dunix -DV40 -DGSI_MBS -D$(GSI_CPU_ENDIAN) -D$(GSI_CPU_PLATFORM)
