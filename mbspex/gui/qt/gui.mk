9f1b703475406e7d5b4c47bd43f984d983282b4e
adamczew <adamczew@bcbf6573-9a26-0410-9ebc-ce4ab7aade96>
2017-06-22 06:10:26 +0000

    mbspex/pexor gui: improve makefiles to compile either against mbspex or pexor/mbspex driver software. Under construction.
    git-svn-id: https://subversion.gsi.de/dabc/drivers@3698 bcbf6573-9a26-0410-9ebc-ce4ab7aade96

--- mbspex/gui/qt/gui.mk
+++ mbspex/gui/qt/gui.mk
@@ -1,5 +1,7 @@
 QMAKE = qmake-qt4
 
+ifndef PEXORSYS
+
 INC = $(MBSROOT)/inc
 
 PEX_BASE=/mbs/driv/mbspex_$(GSI_OS_VERSION)_DEB
@@ -8,5 +10,21 @@ PEX_LIBPATH=$(PEX_BASE)/lib/
 PEX_INCPATH=$(PEX_BASE)/include
 PEX_INC = -I$(PEX_INCPATH)
 PEX_LIBA = $(PEX_LIBPATH)/libmbspex.a
+
+else
+
+#INC = $(PEXORSYS)/user/include
+
+PEX_BASE=/$(PEXORSYS)/user/
+PEX_BINPATH=$(PEX_BASE)/bin/
+PEX_LIBPATH=$(PEX_BASE)/lib/
+PEX_INCPATH=$(PEX_BASE)/include
+PEX_INC = -I$(PEX_INCPATH)
+PEX_LIBA = $(PEX_LIBPATH)/libmbspex.a
+
+
+endif
+
+
 
 CFLAGS  = $(POSIX) -DLinux -Dunix -DV40 -DGSI_MBS -D$(GSI_CPU_ENDIAN) -D$(GSI_CPU_PLATFORM)
