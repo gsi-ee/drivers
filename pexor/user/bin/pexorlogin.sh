bb12aa572ec57a9bf780e8ad47cd7f4ba9640274
adamczew <adamczew@bcbf6573-9a26-0410-9ebc-ce4ab7aade96>
2020-03-09 12:27:09 +0000

    try to add external reference
    git-svn-id: https://subversion.gsi.de/dabc/drivers@4445 bcbf6573-9a26-0410-9ebc-ce4ab7aade96

--- pexor/user/bin/pexorlogin.sh
+++ pexor/user/bin/pexorlogin.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export PEXORSYS=~/drivers/pexor
+export PEXORSYS=~/workspace/drivers/pexor
 export PATH=$PEXORSYS/user/bin:$PATH
 export LD_LIBRARY_PATH=$PEXORSYS/user/lib:$LD_LIBRARY_PATH
 echo pexor environment set for $PEXORSYS 
